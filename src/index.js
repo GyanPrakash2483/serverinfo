8305db7ad6960eaea73eff5b327ce3c5ebc5761a
ezluci <68049793+ezluci@users.noreply.github.com>
2021-07-20 20:35:29 +0300

    added the channelcount command and rewrote some text

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 console.log('Initialising...');
 const Discord = require('discord.js');
 const fs = require('fs');
+const embed_color = '#FD5200';
 client = new Discord.Client();
 
 if (!(require(`${__dirname}/../config.json`).configed)) {
@@ -15,40 +16,66 @@ const {
 	token
 } = require(`${__dirname}/../config.json`);
 
+// We define a channel as a TextChannel, VoiceChannel, NewsChannel and StoreChannel,
+// but discord.js also defines it as a CategoryChannel, which is not really a channel,
+// so we delete all the CategoryChannel s.
+function numberOfChannels(guild)
+{
+	let channels = guild.channels.cache;
+	let size = channels.size;
+	channels.forEach(channel =>
+	{
+		if (channel.type === 'category')
+			size--;
+	})
 
-client.once('ready', () => {
-	console.log('Started');
+	return size;
+}
+
+
+client.on('ready', () => {
+	console.log('Bot started as ' + client.user.username);
 });
-client.login(token);
 
 client.on('message', (message) => {
-	if (!(message.content.startsWith(prefix))) return; //The messsage isn't a command
+	if (!(message.content.startsWith(prefix))) return; // The messsage isn't a command
+
 	let command = message.content.trim().slice(1);
 	if (command === 'serverinfo') {
+
 		message.channel.send(new Discord.MessageEmbed()
-			.setColor('#FD5200')
+			.setColor(embed_color)
 			.setTitle('Server Info')
 			.addFields(
 				{
-					name: 'Server name', value: message.guild.name
+					name: 'Server name',
+					value: message.guild.name
 				},
 				{
-					name: 'Server Creation Date', value: message.guild.createdAt.toString()
+					name: 'Server Creation Date',
+					value: message.guild.createdAt.toString()
 				},
 				{
-					name: 'Number of members', value: message.guild.memberCount
+					name: 'Number of members',
+					value: message.guild.memberCount
 				},
 				{
-					name: 'Server Owner', value: message.guild.owner.toString()
+					name: 'Server Owner',
+					value: message.guild.owner.toString()
 				},
 				{
-					name: 'Server Region', value: message.guild.region[0].toUpperCase() + message.guild.region.slice(1)
+					name: 'Server Region',
+					value: message.guild.region[0].toUpperCase() + message.guild.region.slice(1)
+				},
+				{
+					name: 'Number of channels',
+					value: numberOfChannels(message.guild)
 				}
 			)
 		);
 	} else if (command === 'name') {
 		message.channel.send(new Discord.MessageEmbed()
-			.setColor('#FD5200')
+			.setColor(embed_color)
 			.addFields(
 				{
 					name: 'Server name', value: message.guild.name
@@ -57,7 +84,7 @@ client.on('message', (message) => {
 		);
 	} else if (command === 'creationdate') {
 		message.channel.send(new Discord.MessageEmbed()
-			.setColor('#FD5200')
+			.setColor(embed_color)
 			.addFields(
 				{
 					name: 'Server Creation Date', value: message.guild.createdAt.toString()
@@ -66,7 +93,7 @@ client.on('message', (message) => {
 		);
 	} else if (command === 'membercount') {
 		message.channel.send(new Discord.MessageEmbed()
-			.setColor('#FD5200')
+			.setColor(embed_color)
 			.addFields(
 				{
 					name: 'Number of members', value: message.guild.memberCount
@@ -75,7 +102,7 @@ client.on('message', (message) => {
 		);
 	} else if (command === 'owner') {
 		message.channel.send(new Discord.MessageEmbed()
-			.setColor('#FD5200')
+			.setColor(embed_color)
 			.addFields(
 				{
 					name: 'Server Owner', value: message.guild.owner.toString()
@@ -84,35 +111,49 @@ client.on('message', (message) => {
 		);
 	} else if (command === 'region') {
 		message.channel.send(new Discord.MessageEmbed()
-			.setColor('#FD5200')
+			.setColor(embed_color)
 			.addFields(
 				{
 					name: 'Server Region', value: message.guild.region[0].toUpperCase() + message.guild.region.slice(1)
 				}
 			)
 		);
+	} else if (command === 'channelcount')
+	{
+		message.channel.send(new Discord.MessageEmbed()
+			.setColor(embed_color)
+			.addFields(
+				{
+					name: 'Number of channels',
+					value: numberOfChannels(message.guild)
+				}
+			)
+		)
 	} else if (command === 'help') {
 		message.channel.send(new Discord.MessageEmbed()
-			.setColor('#FD5200')
+			.setColor(embed_color)
 			.setTitle('Help')
 			.addFields(
 				{
-					name: `${prefix}serverinfo`, value: 'Displays all information about the server'
+					name: `${prefix}serverinfo`, value: 'Displays all the information about the server'
 				},
 				{
-					name: `${prefix}name`, value: 'Displays name of server'
+					name: `${prefix}name`, value: 'Displays the name of the server'
 				},
 				{
-					name: `${prefix}creationdate`, value: 'Display server creation date'
+					name: `${prefix}creationdate`, value: 'Displays the server\'s creation date'
 				},
 				{
-					name: `${prefix}membercount`, value: 'Displays number of members'
+					name: `${prefix}membercount`, value: 'Displays the number of members in the server'
 				},
 				{
-					name: `${prefix}owner`, value: 'Owner of the server'
+					name: `${prefix}owner`, value: 'Displays the owner of the server'
 				},
 				{
-					name: `${prefix}region`, value: 'Display server region'
+					name: `${prefix}region`, value: 'Displays server\'s region'
+				},
+				{
+					name: `${prefix}channelcount`, value: 'Displays the number of channels in the server'
 				},
 				{
 					name: `${prefix}help`, value: 'Displays this help section'
@@ -122,3 +163,6 @@ client.on('message', (message) => {
 	}
 
 });
+
+
+client.login(token);
